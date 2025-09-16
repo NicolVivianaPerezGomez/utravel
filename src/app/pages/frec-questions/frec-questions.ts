08084df3fa3612543752a556c908a91f83e334f9
unknown <daavid.nino.cdc@gmail.com>
2025-09-16 16:30:29 -0500

    Sección preguntas y respuestas (usability)

--- src/app/pages/frec-questions/frec-questions.ts
+++ src/app/pages/frec-questions/frec-questions.ts
@@ -1,11 +1,41 @@
-import { Component } from '@angular/core';
+import { ViewportScroller } from '@angular/common';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router, RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-frec-questions',
-  imports: [],
+  imports: [RouterLink],
   templateUrl: './frec-questions.html',
-  styleUrl: './frec-questions.css'
+  styleUrl: './frec-questions.css',
 })
-export class FrecQuestions {
+export class FrecQuestions implements OnInit {
 
+  //lógica del scroll
+  constructor(
+    private route: ActivatedRoute,
+    private router: Router,
+    private viewportScroller: ViewportScroller
+  ) {}
+
+  ngOnInit() {
+    this.route.fragment.subscribe((fragment) => {
+      if (fragment) {
+        setTimeout(() => {
+          this.viewportScroller.scrollToAnchor(fragment);
+        }, 100);
+      }
+    });
+  }
+
+  scrollToFragment(fragment: string, event: Event) {
+    event.preventDefault();
+    this.router
+      .navigate([], {
+        relativeTo: this.route,
+        fragment: fragment,
+      })
+      .then(() => {
+        this.viewportScroller.scrollToAnchor(fragment);
+      });
+  }
 }
